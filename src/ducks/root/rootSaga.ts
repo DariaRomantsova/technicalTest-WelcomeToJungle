17f2ffce2b67e4a78b51f74ae818ccf5becbd019
Daria Romantsova <daria.romantsova@noveogroup.com>
2021-12-09 16:23:28 +0700

    Add rootSaga return type

--- src/ducks/root/rootSaga.ts
+++ src/ducks/root/rootSaga.ts
@@ -1,8 +1,9 @@
-import { all, call } from 'redux-saga/effects';
+import { all, AllEffect, call, CallEffect } from 'redux-saga/effects';
 
 import { watchJobs } from '../jobs/sagas';
 
-export function* rootSaga() {
+export function* rootSaga():Generator<AllEffect<CallEffect<void>>, void, unknown>
+{
   yield all([
     call(watchJobs),
   ]);
